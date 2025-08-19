dd59d2c30101f64f68bf3953cb87eb847fcf64ba
Abhi <abhinav118@gmail.com>
2025-08-19 17:18:28 -0400

    Add components/Navigation.tsx

---
+++ components/Navigation.tsx
@@ -0,0 +1,12 @@
+interface NavigationProps {
+  brand?: any
+  sticky?: any
+}
+
+export default function Navigation({ brand, sticky }: NavigationProps) {
+  return (
+    <div className="bg-white shadow-sm border-b mb-8">
+      {/* Navigation component content */}
+    </div>
+  )
+}
